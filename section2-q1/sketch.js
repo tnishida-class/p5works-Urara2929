ddabbf79377eb0b798ce6faf37636aabf629d99a
1900521h <65203103+Urara2929@users.noreply.github.com>
2020-10-27 10:40:53 +0900

    new_section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,18 @@
 // 小手調べ
 function setup(){
+  createCanvas(200,200);
+}
+
+function draw() {
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    let x = (i + 1)*10;
+    noFill();
+    if(i < 5){
+      stroke(0,0,255);
+      ellipse(100,100,x);
+    } else {
+      stroke(255,0,0);
+      ellipse(100,100,x);
+    }
   }
 }
