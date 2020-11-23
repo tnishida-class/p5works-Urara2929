912420e2174f9b218f0e33a354f62df48d64beea
1900521h <65203103+Urara2929@users.noreply.github.com>
2020-11-23 20:57:15 +0900

    Update section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,17 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",94, 185, 84);
 }
 
-function balloon(t){
+function balloon(t,x,y,z){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
+  fill(x, y, z);
+  noStroke();
   rect(0, 0, w + p * 2, h + p * 2);
+  triangle((w + p * 2) / 2 - p*3 , h + p * 2, (w + p * 2) / 2, h + p * 2 + p*5, (w + p * 2) / 2 + p*3, h + p * 2);
   fill(255);
   text(t, p, h + p);
 }
